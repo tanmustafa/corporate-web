8cdae65c97a8683fe208c464c4b43c134c5536fc
mtan <tan.mustafa.01@gmail.com>
2020-05-26 13:04:19 +0300

    1

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,28 @@
-const path = require('path');
+// const path = require('path');
  
- module.exports = {
-     mode:'none',
-    entry:  './src/index.js',
-    output: {
-        filename: 'build.js',
-        path: path.resolve(__dirname)},
-    module:{
-       rules:[{
-           test: /\.js$/,
-           exclude: /node_modules/,
-           loader: 'babel-loader'
-        }]
-    },
-    devServer:{
-        contentBase : path.resolve(__dirname),
-        historyApiFallback:true
-    }
- }
+//  module.exports = {
+//      mode:'none',
+//     entry:  './src/index.js',
+//     output: {
+//         filename: 'build.js',
+//         path: path.resolve(__dirname)},
+//     module:{
+//        rules:[{
+//            test: /\.js$/,
+//            exclude: /node_modules/,
+//            loader: 'babel-loader'
+//         }]
+//     },
+//     devServer:{
+//         contentBase : path.resolve(__dirname),
+//         historyApiFallback:true
+//     }
+//  }
+
+
+const merge = require('webpack-merge');
++ const common = require('./webpack.common.js');
++
++ module.exports = merge(common, {
++   mode: 'production',
++ });
